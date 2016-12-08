57baf6c5de4600f99b60b770dc5d5ff681328985
dinhhiep36 <dinhhiep.bl@gmail.com>
2016-12-08 22:48:51 +0700

    Update sgmvideo.php

--- web/sgmvideo.php
+++ web/sgmvideo.php
@@ -4,7 +4,7 @@ require "getvideos.php";
 header("Content-type: application/json; charset=utf-8");
 //header("Content-type: text/html; charset=utf-8");
 //header('Access-Control-Allow-Origin: http://sexygirlmedia.blogspot.com');
-header('Access-Control-Allow-Origin: http://www.sexygirlmedia.com');
+header('Access-Control-Allow-Origin: http://sacxinh.blogspot.com');
 header("Access-Control-Allow-Credentials: true ");
 header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
 
